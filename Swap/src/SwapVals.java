a1d03dc858ff20b8e59cd40597c94dc8a550df5b
ozielomega <ozi_guerra@hotmail.com>
2015-05-08 14:09:35 -0700

    first :D

---
+++ Swap/src/SwapVals.java
@@ -0,0 +1,14 @@
+
+public class SwapVals {
+	public static void main(String[] args) {
+		int swapa = 1;
+		int swapb = 2;
+		System.out.print(swapa + " ");
+		System.out.println(swapb);
+		// swap values
+		swapa = swapb;
+		swapb = swapa;
+		System.out.print(swapa + " ");
+		System.out.println(swapb);
+	}
+}
