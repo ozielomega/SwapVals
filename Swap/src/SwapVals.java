e5cf508eb88983a7ddea7365316e86437f14febd
ozielomega <ozi_guerra@hotmail.com>
2015-05-08 14:24:53 -0700

    first changeeee now it works

--- Swap/src/SwapVals.java
+++ Swap/src/SwapVals.java
@@ -1,14 +1,16 @@
-
 public class SwapVals {
 	public static void main(String[] args) {
 		int swapa = 1;
 		int swapb = 2;
-		System.out.print(swapa + " ");
+		int temp = 0;
+		System.out.print(swapa+" ");
 		System.out.println(swapb);
-		// swap values
+		//swap values
+		temp = swapa;
 		swapa = swapb;
-		swapb = swapa;
-		System.out.print(swapa + " ");
+		swapb = temp;
+		System.out.print(swapa+ " ");
 		System.out.println(swapb);
+
 	}
 }
